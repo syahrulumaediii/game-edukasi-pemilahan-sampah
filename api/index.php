c27d667d10f01864d78e0d85f14d9b62464e99cd
syahrulumaediii <syahrulumaediii@gmail.com>
2026-09-02 21:17:12 +0700

    Deploy vercel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
 
-// Forward Vercel serverless requests to Laravel public/index.php
+// Forward Vercel serverless requests to Laravel
 // Prepare temporary directories in /tmp because Vercel filesystem is read-only
 
 $tmpStorage = '/tmp/storage';
@@ -19,4 +19,22 @@ foreach ($dirs as $dir) {
     }
 }
 
-require __DIR__ . '/../public/index.php';
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var \Illuminate\Foundation\Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath($tmpStorage);
+
+$app->handleRequest(\Illuminate\Http\Request::capture());
+
